baf5c4d9aac27600d69f50920cacbb3169abc7fc
Dipak Jotaneeya <dipak.jotaneeya18@gmail.com>
2018-08-20 14:42:16 +0530

    Add files via upload

---
+++ src/android/DownloadBase64.java
@@ -0,0 +1,100 @@
+package com.deejo.cordova.plugin.DownloadBase64;
+
+// The native APIs
+import android.os.Environment;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+// Cordova-required packages
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DownloadBase64 extends CordovaPlugin {
+
+  String filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+  
+  @Override
+  public boolean execute(String action, JSONArray args,
+    final CallbackContext callbackContext) {
+      // Verify that the user sent a 'show' action
+      if (!action.equals("show")) {
+        callbackContext.error("\"" + action + "\" is not a recognized action.");
+        return false;
+      }
+	  
+      String url;
+	  String fileName;
+	  
+      try {
+        JSONObject options = args.getJSONObject(0);
+        url = options.getString("url");
+        filePath = filePath + options.getString("folderName");
+		fileName = options.getString("fileName");
+      } catch (JSONException e) {
+        callbackContext.error("Error encountered: " + e.getMessage());
+        return false;
+      }
+	  
+	  // // Create the toast
+      // Toast toast = Toast.makeText(cordova.getActivity(), message,
+	  // DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
+      // // Display toast
+      // toast.show();
+      
+	  downloadFile(url, filePath, fileName); 
+  }
+  
+  public void downloadFile(final String url, final String filePath, final String fileName) {
+
+        Thread networkThread = new Thread() {
+
+            @Override
+            public void run() {
+
+                File outputFile = new File(filePath, fileName);
+
+                try {
+                    URL u = new URL(url);
+                    URLConnection conn = u.openConnection();
+                    int contentLength = conn.getContentLength();
+
+                    DataInputStream stream = new DataInputStream(u.openStream());
+
+                    byte[] buffer = new byte[contentLength];
+                    stream.readFully(buffer);
+                    stream.close();
+
+                    DataOutputStream fos = new DataOutputStream(new FileOutputStream(outputFile));
+                    fos.write(buffer);
+                    fos.flush();
+                    fos.close();
+					
+					// Send a positive result to the callbackContext
+					PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+					callbackContext.sendPluginResult(pluginResult);
+					return true;
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    return;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        };
+
+        networkThread.start();
+    }
+  
+}
---
+++ www/downloadBase64.js
@@ -0,0 +1,24 @@
+// Empty constructor
+function DownloadBase64() {}
+
+// The function that passes work along to native shells
+// @param url        URL of the file from where it has to be downloaded
+// @param folderName folderName, under which file has to be downloaded (under Download directory)
+// @param fileName   Name of the file with which it has to be saved (filename.extension)
+DownloadBase64.prototype.show = function(url, filePath, fileName, successCallback, errorCallback) {
+  var options = {};
+  options.url = url;
+  options.filePath = filePath;
+  options.fileName = fileName;
+  cordova.exec(successCallback, errorCallback, 'DownloadBase64', 'show', [options]);
+}
+
+// Installation constructor that binds DownloadBase64 to window
+DownloadBase64.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.downloadBase64 = new DownloadBase64();
+  return window.plugins.downloadBase64;
+};
+cordova.addConstructor(DownloadBase64.install);
